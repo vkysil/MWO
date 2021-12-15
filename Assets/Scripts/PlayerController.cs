77355b5299e35e1f1bf521097095bf1f6e811f02
pwr204507 <204507@student.pwr.edu.pl>
2021-12-15 08:30:25 +0100

    Scene update, controller cleanup
    Scene update, controller cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,26 +6,37 @@ using UnityEngine.InputSystem;
 // Controller for player-specific movement
 public class PlayerController : MonoBehaviour
 {
-    // Player-specific properties
+
+    #region public properties
+    [Header("Player Properties")]
     public float walkSpeed = 10f;
     public float gravity = 20f;
     public float jumpSpeed = 15f;
     public float doubleJumpSpeed = 15f;
+    public float xWallJumpSpeed = 15f;
+    public float yWallJumpSpeed = 15f;
 
+    [Header("Player Abilities")]
     // Player ability toggles
     public bool canDoubleJump;
+    public bool canWallJump;
+    public bool canDoubleJumpAfterWallJump;
 
     // Player state
+    [Header("Player State")]
     public bool isJumping; // true when the player is jumping
     public bool isDoubleJumping; // true when the player is double-jumping
+    public bool isWallJumping; // true when the player is wall-jumping
+    #endregion
 
-    // Input properties
+    #region private properties
     private bool _startJump; // true when jump button started
     private bool _releaseJump; // true when jump button released
 
     private Vector2 _input;
     private Vector2 _moveDirection;
     private CharacterController2D _characterController;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -37,63 +48,128 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        // Update x dimension
-        _moveDirection.x = _input.x;
-        _moveDirection.x *= walkSpeed;
-
-        if(_moveDirection.x < 0)
-        {
-            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-        } else if(_moveDirection.x > 0)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        }
+        ProcessHorizontalMovement();
 
         // Update y dimension
         if (_characterController.below) // ground collision detected
         {
-            _moveDirection.y = 0f;
-
-            isJumping = false;
-            isDoubleJumping = false;
-
-            if(_startJump) // jumping event initiated
-            {
-                _startJump = false;
-                _moveDirection.y = jumpSpeed;
-                isJumping = true;
-                _characterController.DisableGroundCheck();
-            }
+            OnGround();
         }
         else // no ground collision detected
         {
-            // smaller jump when jump button was released early
-            if(_releaseJump)
-            {
-                _releaseJump = false;
-                if(_moveDirection.y > 0)
-                {
-                    _moveDirection.y *= 0.5f;
-                }
-            }
-
-            // double-jumping
-            if (_startJump)
-            {
-                if(canDoubleJump && (!_characterController.left && !_characterController.right))
-                {
-                    if (!isDoubleJumping)
-                    {
-                        _moveDirection.y = doubleJumpSpeed;
-                        isDoubleJumping = true;
-                    }
-                }
-                _startJump = false;
-            }
-
-            GravityCalculations();
+            InAir();
         }
         _characterController.Move(_moveDirection * Time.deltaTime);
+    }
+
+    void ProcessHorizontalMovement()
+    {
+        // Update x dimension
+        // unless during a wall-jump
+        if (!isWallJumping)
+        {
+            _moveDirection.x = _input.x;
+            _moveDirection.x *= walkSpeed;
+            if (_moveDirection.x < 0)
+            {
+                transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+            else if (_moveDirection.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+        }
+    }
+
+    // method responsible for interactions while collision below character is detected
+    void OnGround()
+    {
+        // clear any downward motion while on ground
+        _moveDirection.y = 0f;
+
+        ClearAirAbilityFlags();
+
+        Jump();
+
+    }
+
+    // clear flags for all in-air abilities
+    void ClearAirAbilityFlags()
+    {
+        isJumping = false;
+        isDoubleJumping = false;
+        isWallJumping = false;
+    }
+
+    // method for jumping-related interactions
+    void Jump()
+    {
+        if (_startJump) // jumping event initiated
+        {
+            _startJump = false;
+            _moveDirection.y = jumpSpeed;
+            isJumping = true;
+            _characterController.DisableGroundCheck();
+        }
+    }
+
+    // method responsible for interactions while collision below character is NOT detected
+    void InAir()
+    {
+        AirJump();
+
+        GravityCalculations();
+    }
+
+    // method for jump-related interactions while in-air
+    void AirJump()
+    {
+        // smaller jump when jump button was released early
+        if (_releaseJump)
+        {
+            _releaseJump = false;
+            if (_moveDirection.y > 0)
+            {
+                _moveDirection.y *= 0.5f;
+            }
+        }
+
+        // pressed jump while in the air
+        if (_startJump)
+        {
+            // double-jump
+            if (canDoubleJump && (!_characterController.left && !_characterController.right))
+            {
+                if (!isDoubleJumping)
+                {
+                    _moveDirection.y = doubleJumpSpeed;
+                    isDoubleJumping = true;
+                }
+            }
+            // wall-jump
+            else if (canWallJump && (_characterController.left || _characterController.right))
+            {
+                if (_moveDirection.x <= 0 && _characterController.left)
+                {
+                    _moveDirection.x = xWallJumpSpeed;
+                    _moveDirection.y = yWallJumpSpeed;
+                    transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                }
+                else if (_moveDirection.x >= 0 && _characterController.right)
+                {
+                    _moveDirection.x = -xWallJumpSpeed;
+                    _moveDirection.y = yWallJumpSpeed;
+                    transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                }
+
+                StartCoroutine("WallJumpWaiter"); // how soon after wall-jump player regains control over x-movement
+                if (canDoubleJumpAfterWallJump)
+                {
+                    isDoubleJumping = false;
+                }
+            }
+            _startJump = false;
+        }
     }
 
     // gravity behaviour while jumping
@@ -106,7 +182,7 @@ public class PlayerController : MonoBehaviour
         _moveDirection.y -= gravity * Time.deltaTime;
     }
 
-    // Input methods
+    #region input methods
     // Method is called on each input event
     public void OnMovement(InputAction.CallbackContext context)
     {
@@ -126,5 +202,14 @@ public class PlayerController : MonoBehaviour
             _startJump = false;
         }
     }
+    #endregion
 
+    #region coroutines
+    IEnumerator WallJumpWaiter()
+    {
+        isWallJumping = true;
+        yield return new WaitForSeconds(0.4f);
+        isWallJumping = false;
+    }
+    #endregion
 }
